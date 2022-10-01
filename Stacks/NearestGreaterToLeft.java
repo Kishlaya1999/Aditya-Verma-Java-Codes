abe34e823030f9cd33371639bf4cafc7c0bffce1
Kishlaya1999 <kishlaya54321@gmail.com>
2022-10-01 18:23:11 +0530

    Correction Done Nearest Greater to Left was named as Nearest Greater to right and vice versa

--- Stacks/NearestGreaterToLeft.java
+++ Stacks/NearestGreaterToLeft.java
@@ -1,5 +1,4 @@
 import java.util.Stack;
-
 import javax.lang.model.element.Element;
 
 public class NearestGreaterToLeft {
@@ -16,21 +15,21 @@ public class NearestGreaterToLeft {
      private static int[] NGL(int[] arr) {
           Stack<Integer> st = new Stack<>();
           int[] ans = new int[arr.length];
-          int k = arr.length - 1;
+          int k = 0;
 
-          for (int i = arr.length - 1; i >= 0; i--) {
+          for (int i = 0; i < arr.length; i++) {
                if (st.size() == 0) {
-                    ans[k--] = -1;
+                    ans[k++] = -1;
                } else if (st.size() > 0 && st.peek() > arr[i]) {
-                    ans[k--] = st.peek();
+                    ans[k++] = st.peek();
                } else if (st.size() > 0 && st.peek() <= arr[i]) {
                     while (st.size() > 0 && st.peek() <= arr[i]) {
                          st.pop();
                     }
                     if (st.size() == 0) {
-                         ans[k--] = -1;
+                         ans[k++] = -1;
                     } else {
-                         ans[k--] = st.peek();
+                         ans[k++] = st.peek();
                     }
                }
                st.push(arr[i]);
--- Stacks/NearestGreaterToRight.java
+++ Stacks/NearestGreaterToRight.java
@@ -1,5 +1,40 @@
-public class NearestGreaterToRight {
-     public static void main(String[] args) {
+import java.util.Stack;
 
+public class NearestGreaterToRight {
+
+     public static void main(String[] args) {
+          int[] arr = { 1, 3, 2, 4 };
+          int[] ngr = NGR(arr);
+
+          for (int i : ngr) {
+               System.out.print(i + " ");
+          }
      }
+
+     private static int[] NGR(int[] arr) {
+          Stack<Integer> st = new Stack<>();
+          int[] ans = new int[arr.length];
+          int k = arr.length - 1;
+
+          for (int i = arr.length - 1; i >= 0; i--) {
+               if (st.size() == 0) {
+                    ans[k--] = -1;
+               } else if (st.size() > 0 && st.peek() > arr[i]) {
+                    ans[k--] = st.peek();
+               } else if (st.size() > 0 && st.peek() <= arr[i]) {
+                    while (st.size() > 0 && st.peek() <= arr[i]) {
+                         st.pop();
+                    }
+                    if (st.size() == 0) {
+                         ans[k--] = -1;
+                    } else {
+                         ans[k--] = st.peek();
+                    }
+               }
+               st.push(arr[i]);
+          }
+
+          return ans;
+     }
+
 }
