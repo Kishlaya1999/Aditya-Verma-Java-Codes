2e52a81d53242eb2c081eac884c833acf3ebfae9
Kishlaya1999 <kishlaya54321@gmail.com>
2022-10-02 11:53:26 +0530

    Added comments in stock span problem

---
+++ Stacks/RainWaterTrapping.java
@@ -0,0 +1,36 @@
+public class RainWaterTrapping {
+
+     public static void main(String[] args) {
+          int[] arr = { 6, 7, 5, 2, 4, 5, 9, 3 };
+          System.out.println(rainWaterTrapping(arr));
+     }
+
+     private static int rainWaterTrapping(int[] arr) {
+
+          int[] maxInLeft = new int[arr.length];
+          int[] maxInRight = new int[arr.length];
+
+          maxInLeft[0] = arr[0];
+          for (int i = 1; i < arr.length; i++) {
+               maxInLeft[i] = Math.max(maxInLeft[i - 1], arr[i]);
+          }
+
+          maxInRight[arr.length - 1] = arr[arr.length - 1];
+          for (int i = arr.length - 2; i >= 0; i--) {
+               maxInRight[i] = Math.max(maxInRight[i + 1], arr[i]);
+          }
+
+          int[] water = new int[arr.length];
+          for (int i = 0; i < water.length; i++) {
+               water[i] = Math.min(maxInLeft[i], maxInRight[i]) - arr[i];
+          }
+
+          int amoutOfWater = 0;
+          for (int i = 0; i < water.length; i++) {
+               amoutOfWater += water[i];
+          }
+
+          return amoutOfWater;
+     }
+
+}
--- Stacks/StockSpan.java
+++ Stacks/StockSpan.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+// import javafx.util.Pair;
+
 public class StockSpan {
 
      public static void main(String[] args) {
@@ -9,7 +12,47 @@ public class StockSpan {
           }
      }
 
+     // Based on the next greater to left (NGL)
      private static int[] stockSpan(int[] arr) {
-          return null;
+          // Creating a stack which will contain array
+          // and the array will contain the NGL and its index [100,0],[80,1]......
+          Stack<int[]> st = new Stack<>();
+          // Creating the array answer which will contain index of the next greter to left
+          // and we will create our own answer using next greater to left index and
+          // current index
+          int[] ans = new int[arr.length];
+          int k = 0;
+          for (int i = 0; i < arr.length; i++) {
+               // if stack is empty it means NGL doesn't exist so inserting -1 for that index
+               if (st.size() == 0) {
+                    ans[k++] = -1;
+               }
+               // if stack is not empty and top of stack [0] i.e NGL is greater than arr[i]
+               // then inserting NGL's index
+               else if (st.size() > 0 && st.peek()[0] > arr[i]) {
+                    ans[k++] = st.peek()[1];
+               }
+               // other wise popping the elements from the stack till we get the NGL and
+               // repeting the above two operation (if , else if)
+               else if (st.size() > 0 && st.peek()[0] <= arr[i]) {
+                    while (st.size() > 0 && st.peek()[0] <= arr[i]) {
+                         st.pop();
+                    }
+                    if (st.size() == 0) {
+                         ans[k++] = -1;
+                    } else {
+                         ans[k++] = st.peek()[1];
+                    }
+               }
+               // Before moving forward we insert the current value and its index for further
+               // checking
+               st.push(new int[] { arr[i], i });
+          }
+          // Creating the answer on the basis of the NGL index i.e finding the difference
+          // of current index and NGL's index
+          for (int i = 0; i < ans.length; i++) {
+               ans[i] = i - ans[i];
+          }
+          return ans;
      }
 }
